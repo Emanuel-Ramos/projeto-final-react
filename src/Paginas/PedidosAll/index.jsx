aaefcee5a3a601d328ae9ed9c25133f337aab3ed
GuilhermeSantosmain <guilhermesantosmain@gmail.com>
2021-06-27 17:41:59 -0300

    pedidos

--- src/Paginas/PedidosAll/index.jsx
+++ src/Paginas/PedidosAll/index.jsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react"
+import PedidoCard from "../../Components/PedidoCard"
+import http from "../../http"
 
 
 const PedidosAll = () => {
+    const [pedidos, setPedidos] = useState([])
+    useEffect(() => {
+
+        http.get('carrinho').then((response) => { setPedidos(response.data) }).catch(erro => console.log(erro))
+
+    }, [])
+
     return (
-        <div></div>
+        <div>
+            <div className="container">
+                    <div className="cards-area">
+                        {pedidos.map((item) =>
+                            <PedidoCard key={item.id} id={item.id} produtos={item.produtos} valorTotal={item.valor} cliente={item.cliente} codigo={item.codigo} />
+                        )}
+                    </div>
+                </div>
+
+        </div>
 
     )
 }
